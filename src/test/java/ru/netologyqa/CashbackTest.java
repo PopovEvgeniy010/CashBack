2b839850804d3f0fede8636600805d20048da338
Evgeniy Popov <evgenij.selin.94@bk.ru>
2023-03-09 18:08:10 +0300

    Change junit4 commit

--- src/test/java/ru/netologyqa/CashbackTest.java
+++ src/test/java/ru/netologyqa/CashbackTest.java
@@ -1,18 +1,28 @@
 package ru.netologyqa;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackTest {
+        @Test
+        public void remain () {
+            int amount = 900;
+            int expected = 100;
+            Cashback service = new Cashback();
+            int actual = service.remain(amount);
+            assertEquals(expected, actual);
+        }
 
-    @Test
-    public void testRemain() {
-        int amount = 900;
-        int expected = 100;
-        Cashback service = new Cashback();
-        int actual = service.remain(amount);
-        assertEquals(actual,expected);
-
+        @Test
+        public void remain1 () {
+            int amount = 1000;
+            int expected = 0;
+            Cashback service = new Cashback();
+            int actual = service.remain(amount);
+            assertEquals(expected, actual);
+        }
     }
-}
+
+
+
